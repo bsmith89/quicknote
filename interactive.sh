0664cbdd4857a12866f3a592ff6a3f549d388129
Byron Smith <bsmith89@gmail.com>
2015-08-18 19:50:46 -0400

    Improve debugging assistant.

--- interactive.sh
+++ interactive.sh
@@ -1,2 +1,7 @@
+# For interactive debugging.
+# Just source this script to make library functions available.
+# You'll have to source user configuration files separately.
+
 export QUICKNOTE_DIR=$PWD
 source lib.sh
+source quicknote.cfg
